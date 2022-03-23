d6d14f3295d4a946f70c8b4fc936c9cdba2e10d1
0xtter <41399587+0xtter@users.noreply.github.com>
2022-03-23 17:17:35 +0100

    23/03 Avancement

--- EllipticCurveGraphicInterface/scripts/EllipticScript.js
+++ EllipticCurveGraphicInterface/scripts/EllipticScript.js
@@ -12,25 +12,8 @@ function main() {
     graph1.addDynamicLine(graph1.points["1"],graph1.points["2"],p1);
     graph1.saveGraphicState();
     console.log(graph1.getExpressionById('a_1'))
-    i = 0
-
-
-    // a.observe('numericValue', function (evt) {
-    //     var calculatorRect = graph1.element.getBoundingClientRect();
-    //     graph1.element.addEventListener('mousemove', function (evt) {
-    //         pos=graph1.calculator.pixelsToMath({
-    //                 x: evt.clientX - calculatorRect.left,
-    //                 y: evt.clientY - calculatorRect.top
-    //             });
-    //             graph1.updateLine(1,1,1);
-    //     },{ once: true });
-    // });
-    // window.setInterval(function () {
-    //     graph1.updatePoint(1,[Math.cos(i/100),Math.sin(i/100)])
-    //     graph1.updateLine(1,Math.cos(i/100),Math.sin(i/100))
-    //     i = (i+1)%(Math.PI*200)
-    //     //console.log(graph1.calculator.getExpressions())
-    // },15);
+    
+    
     document.getElementById('button1').onclick = function () {
         graph1.loadGraphicState();
     };
